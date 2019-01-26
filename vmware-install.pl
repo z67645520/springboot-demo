c5364646cf72554a1d729cb86e9c44dbe1939139
sun <z67645520@126.com>
2019-01-26 12:10:16 +0800

    全部

--- vmware-install.pl
+++ vmware-install.pl
@@ -5,4 +5,4 @@ dir=`dirname $0`
 echo "VMware Tools installation start `date`" >> ${LOGFILE}
 ${dir}/vmware-install.real.pl "$@" 2>&1 | tee -a ${LOGFILE}
 echo "VMware Tools installation end `date`" >> ${LOGFILE}
-
+xiug
